d23be5197c6ffe69dde39f0297e8f9102c5d52d1
DESKTOP-T940B83\leona <leonardodoprado231@gmail.com>
2023-04-04 00:23:37 -0300

    Att 04/04: atualizando projeto

--- frontend/src/app/components/header/styles.ts
+++ frontend/src/app/components/header/styles.ts
@@ -4,12 +4,12 @@ export const ContainerHeader = styled.div`
     width: 100vw;
     height: 60px;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: center;
-    background: #46B241;
+    background: #23242F;
     margin-right: auto;
     margin-left: auto;
-    font-family: 'Figtree', sans-serif;
+    font-family: 'Roboto', sans-serif;
     box-shadow: 0px 0px 8px gray;
 
     nav {
@@ -23,15 +23,11 @@ export const ContainerHeader = styled.div`
         display: flex;
         align-items: center;
         font-weight: 700;
-        margin-left: 20px;
         transition-duration: 0.3s;
     }
 
     a:hover {
         color: #DCDCDC;
-        box-shadow: 0 0 10px #46B241;
-        -webkit-transform: scale(1.1);
-        transform: scale(1.1);
     }
 
     img {
--- frontend/src/app/pages/home/Home/index.tsx
+++ frontend/src/app/pages/home/Home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from '../../../components/header'
 import Footer from '../../../components/footer'
+import { ContainerMain } from '../Home/style'
 
 const HomePage = () => {
   return (
     <>
         <Header />
+        <ContainerMain>
+          <h1>Home</h1>
+        </ContainerMain>
+        <Footer />
     </>
   )
 }
