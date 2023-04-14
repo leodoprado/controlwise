e6208334b4763dcd0114473b67c771ad50b16aad
DESKTOP-T940B83\leona <leonardodoprado231@gmail.com>
2023-04-14 01:08:33 -0300

    Att 14-04: Correções front

--- frontend/src/app/pages/home/Home/style.ts
+++ frontend/src/app/pages/home/Home/style.ts
@@ -4,18 +4,17 @@ export const ContainerMain = styled.div`
     height: 100vh;  
     background: rgb(55,54,65);
     background: linear-gradient(0deg, rgba(55,54,65,1) 9%, rgba(35,36,47,1) 31%, rgba(17,16,23,1) 82%);
-    display: flex;
     font-family: 'Comfortaa', sans-serif;
     color: white;
-     
+`
 
-    #div-main {
-        width: 50%;
-        display: flex;
-        flex-flow: column wrap;
-        align-items: center;
-        justify-content: center;
-    }
+export const ContainerContent = styled.div`
+    height: 100vh;
+    padding: 0px 10%;
+    display: flex;
+    flex-flow: column wrap;
+    align-items: flex-start;
+    justify-content: center;
 
     h1 {
         font-weight: 7800;
@@ -38,4 +37,4 @@ export const ContainerMain = styled.div`
         background-color: #1b3ca2;
         transition: background-color 200ms linear;
     }
-`
+` 
