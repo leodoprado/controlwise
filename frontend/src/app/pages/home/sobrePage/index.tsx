1fee5256d936966a094fc448c64319c957a55d85
Leo <leonardodoprado231@gmail.com>
2023-06-06 19:50:57 -0300

    Att: about page

--- frontend/src/app/pages/home/sobrePage/index.tsx
+++ frontend/src/app/pages/home/sobrePage/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "../../../components/header";
 import Footer from "../../../components/footer";
-import { ContainerMain, ContainerContent } from "./style";
+import { ContainerMain, ContainerContent, ContainerImage } from "./style";
+import AboutImage from '../../../../assets/imagem-sobre.png'
 
 
 const SobrePage = () => {
@@ -9,16 +10,12 @@ const SobrePage = () => {
         <>
             <Header />
                 <ContainerMain>
-                    <ContainerContent>
-                    <div id="infoBox">
-                        <h1>
-                            Sobre
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed velit in purus elementum eleifend. 
-                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames. 
-                        </p> 
-                    </div>
+                <ContainerImage>
+                    <img src={AboutImage} alt="Imagem Main"></img>
+                </ContainerImage>
+                <ContainerContent>
+                        <h1>Sobre suas funções!</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis dolore quibusdam sequi porro hic recusandae corporis, nisi dolores officia ipsum enim, maiores architecto nemo tempora? Quisquam dicta expedita culpa enim? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro corrsasaupti sapiente beatae ut obcaecati veniam ad nulla expedita consectetur perferendis praesentium, aliquam aperiam reprehenderit quisquam repellat earum sed. Labore, cum. Lore</p>
                     </ContainerContent>
                 </ContainerMain>
             <Footer />
--- frontend/src/app/pages/home/sobrePage/style.ts
+++ frontend/src/app/pages/home/sobrePage/style.ts
@@ -2,38 +2,62 @@ import styled from "styled-components";
 
 export const ContainerMain = styled.div`
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
+    padding: 0px 10%;
     flex-grow: 1;
     background: rgb(55,54,65);
     background: linear-gradient(0deg, rgba(55,54,65,1) 9%, rgba(35,36,47,1) 31%, rgba(17,16,23,1) 82%);
-    font-family: 'Comfortaa', sans-serif;
+    font-family: 'Mulish', sans-serif;
     color: white;
 `
 
 export const ContainerContent = styled.div`
     height: 100%;
-    padding: 0px 10%;
     display: flex;
     flex-flow: column wrap;
     align-items: flex-start;
     justify-content: center;
-
-    #infoBox {
-        border-style: solid;
-        border-color: white;
-        border-left-width: 2px;
-        margin-left: 15px;
-        padding-left: 15px;
-    }
+    padding-left: 15px;
 
     h1 {
-        font-weight: 7800;
-        font-size: 4.5ex;
-        margin-bottom: 20px;
+        font-weight: 800;
+        font-size: 2.5rem;
+        line-height: 55px;
+        text-transform: uppercase;
     }
-    
+
     p {
-        font-size: 2ex;
+        font-size: 22px;
+        text-align: justify;
+    }
+
+    button {
+        font-weight: 800;
+        margin-top: 3rem;
+        width: 180px;
+        height: 3rem;
+        font-size: 2.5ex;
+        background-color: #20c4a3;
+        color: white;
+        border-style: none;
+        border-radius: 5px;
+        cursor:pointer;
+    }
+
+    button:hover {
+        background-color: #0085bd;
+        transition: 0.6s;
+    }
+` 
+
+export const ContainerImage = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 440px;
+        height: 440px;
     }
 `
