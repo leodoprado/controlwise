517860eaecf4530f76ac644fd130b96a89bdaa3a
Eduardo Pottker <eduardopottkerdasilva@gmail.com>
2023-06-12 18:57:08 -0300

    Alterações Tela Dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("controlwise", "root", "senha123", {
+const sequelize = new Sequelize("controlwise", "root", "1234", {
     host:'localhost',
     dialect: 'mysql'
 })
