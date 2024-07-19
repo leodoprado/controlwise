79fccb77cb991567262faf8a50246df17d95560e
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-07-18 22:05:33 -0300

    feat: restructuring

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GlobalStyle from './style/globalStyles'
+import GlobalStyle from './styles'
 import App from "./app"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { queryClient } from './lib/react-query'
