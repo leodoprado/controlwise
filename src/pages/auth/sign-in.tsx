f6894f3fc6fc3e9d33723a356f94afb727de2a44
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-08-30 00:29:16 -0300

    feat: att

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,12 +1,45 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import logo from '@/assets/logo.svg'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInForm = z.object({
+  email: z.string().email(),
+  senha: z.string(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Usuário autenticado com sucesso!', {
+        action: {
+          label: 'Login',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Erro ao autenticar usuário.')
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -20,22 +53,31 @@ export function SignIn() {
             />
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="senha">Senha</Label>
-              <Input id="senha" type="password" />
+              <Input id="senha" type="password" {...register('senha')} />
             </div>
 
-            <Button className="w-full font-bold" type="submit">
+            <Button
+              disabled={isSubmitting}
+              className="w-full font-bold"
+              type="submit"
+            >
               Acessar painel
             </Button>
-            <Button variant="ghost" className="w-full">
-              <Link to="/signup">Nova Conta</Link>
+            <Button variant="outline" className="w-full p-0">
+              <Link
+                to="/signup"
+                className="flex h-full w-full items-center justify-center p-0"
+              >
+                Nova Conta
+              </Link>
             </Button>
           </form>
         </div>
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,12 +1,47 @@
 import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import logo from '@/assets/logo.svg'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signUpForm = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  senha: z.string(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
 export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Usuário cadastrado com sucesso!', {
+        action: {
+          label: 'Login',
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
+        },
+      })
+    } catch (error) {
+      toast.error('Erro ao cadastrar usuário.')
+    }
+  }
   return (
     <>
       <Helmet title="Login" />
@@ -20,27 +55,36 @@ export function SignUp() {
             />
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="nome">Nome</Label>
-              <Input id="nome" type="text" />
+              <Input id="nome" type="text" {...register('name')} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="senha">Senha</Label>
-              <Input id="senha" type="password" />
+              <Input id="senha" type="password" {...register('senha')} />
             </div>
 
-            <Button className="w-full font-bold" type="submit">
+            <Button
+              disabled={isSubmitting}
+              className="w-full font-bold"
+              type="submit"
+            >
               Cadastrar
             </Button>
-            <Button variant="ghost" className="w-full">
-              <Link to="/signin">Voltar</Link>
+            <Button variant="outline" className="w-full p-0">
+              <Link
+                to="/signin"
+                className="flex h-full w-full items-center justify-center p-0"
+              >
+                Voltar
+              </Link>
             </Button>
           </form>
         </div>
