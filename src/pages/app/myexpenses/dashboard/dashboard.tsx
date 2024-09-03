ae737e0a3c7fc38dea3a1aef2eda8fb3d92043a8
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-09-02 21:07:19 -0300

    feat: att 02-09

--- src/pages/app/myexpenses/dashboard/dashboard.tsx
+++ src/pages/app/myexpenses/dashboard/dashboard.tsx
@@ -1,11 +1,19 @@
 import { Helmet } from 'react-helmet-async'
 
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title="Painel" />
       <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Painel</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
       </div>
     </>
   )
