fd97ba39f04b0ac34180d1c0ee7545050bc0d972
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-09-17 00:58:42 -0300

    feat: transaction page

--- src/pages/app/myexpenses/transactions/transactions.tsx
+++ src/pages/app/myexpenses/transactions/transactions.tsx
@@ -1,7 +1,13 @@
-import { X } from 'lucide-react'
+import { Edit, Menu, Trash, TrendingDown } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import {
   Table,
   TableBody,
@@ -21,14 +27,12 @@ export function ETransactionsPage() {
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[64px]">Tipo</TableHead>
-                <TableHead className="w-[140px]">Categoria</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
+                <TableHead className="w-1/6">Tipo</TableHead>
+                <TableHead className="w-1/6">Categoria</TableHead>
+                <TableHead className="w-1/5">Realizado em</TableHead>
+                <TableHead className="w-1/6">Valor</TableHead>
+                <TableHead className="w-1/5">Status</TableHead>
+                <TableHead className="w-1/6">Opções</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
@@ -36,13 +40,19 @@ export function ETransactionsPage() {
                 return (
                   <TableRow key={i}>
                     <TableCell className="font-mono text-xs font-medium">
-                      Despesa
+                      <div className="flex items-center gap-2">
+                        <TrendingDown className="h-4 w-4 text-red-500" />
+                        Despesa
+                      </div>
                     </TableCell>
                     <TableCell className="font-mono text-xs font-medium">
-                      821e78f7asdhdf128h
+                      Casa
                     </TableCell>
                     <TableCell className="text-muted-foreground">
-                      há 15 minutos
+                      19/09/2024
+                    </TableCell>
+                    <TableCell className="font-medium text-red-500">
+                      R$ 149,90
                     </TableCell>
                     <TableCell>
                       <div className="flex items-center gap-2">
@@ -52,15 +62,25 @@ export function ETransactionsPage() {
                         </span>
                       </div>
                     </TableCell>
-                    <TableCell className="font-medium">
-                      Diego Schell Fernandes
-                    </TableCell>
-                    <TableCell className="font-medium">R$ 149,90</TableCell>
                     <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancelar
-                      </Button>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" size="xs">
+                            <Menu className="h-6 w-6" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                          align="end"
+                          className="border-5 mt-1 w-56 transform p-0 shadow-2xl transition-all duration-500 ease-in-out"
+                        >
+                          <DropdownMenuItem className="cursor-pointer gap-2 p-3">
+                            <Edit className="h-4 w-4" /> Editar Movimentação
+                          </DropdownMenuItem>
+                          <DropdownMenuItem className="cursor-pointer gap-2 p-3">
+                            <Trash className="h-4 w-4" /> Excluir Movimentação
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
                     </TableCell>
                   </TableRow>
                 )
