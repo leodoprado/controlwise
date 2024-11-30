e22c596a184a56e334e576ff66949ba608e77d87
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-11-30 09:04:42 -0300

    feat: add planning

--- src/api/POST/post-planning.ts
+++ src/api/POST/post-planning.ts
@@ -3,17 +3,20 @@ import { api } from '@/lib/axios'
 export interface CreatePlanningBody {
   titulo: string
   valorTarget: number
+  tipo: 'DESPESA' | 'RECEITA'
   categoryId: string
 }
 
 export async function createPlanning({
   titulo,
   valorTarget,
+  tipo,
   categoryId,
 }: CreatePlanningBody) {
   await api.post('/planning', {
     titulo,
     valorTarget,
+    tipo,
     categoryId,
   })
 }
--- src/pages/app/myexpenses/_components/add-planning.tsx
+++ src/pages/app/myexpenses/_components/add-planning.tsx
@@ -1,5 +1,12 @@
 import { useMutation } from '@tanstack/react-query'
-import { Banknote, Bookmark, FilePen, Presentation } from 'lucide-react'
+import {
+  Banknote,
+  Bookmark,
+  FilePen,
+  Presentation,
+  TrendingDown,
+  TrendingUp,
+} from 'lucide-react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -16,6 +23,7 @@ import {
 } from '@/components/ui/card'
 import { DialogContent, DialogFooter } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import { CategorySelector } from './categorySelector'
 
@@ -29,6 +37,7 @@ function insertMaskInMoney(value: string): string {
 const planningSchema = z.object({
   titulo: z.string(),
   valorTarget: z.string(),
+  tipo: z.enum(['RECEITA', 'DESPESA']),
   categoryId: z.string(),
 })
 
@@ -37,7 +46,9 @@ type PlanningSchema = z.infer<typeof planningSchema>
 export function AddPlanning() {
   const [selectedTitulo, setSelectedTitulo] = useState<string>('')
   const [selectedCategoryId, setSelectedCategoryId] = useState<string>('')
-
+  const [planningType, setPlanningType] = useState<'DESPESA' | 'RECEITA'>(
+    'DESPESA',
+  )
   const {
     register,
     handleSubmit,
@@ -60,6 +71,7 @@ export function AddPlanning() {
       await createPlanningFn({
         titulo: selectedTitulo,
         valorTarget,
+        tipo: planningType,
         categoryId: selectedCategoryId,
       })
 
@@ -75,87 +87,119 @@ export function AddPlanning() {
 
   return (
     <DialogContent className="mx-auto w-[400px] max-w-[90vw] p-6">
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center text-xl font-semibold">
-            <Presentation className="mr-2 h-5 w-5 text-gray-700" />
-            Planejamento Mensal
-          </CardTitle>
-        </CardHeader>
+      <Tabs
+        defaultValue="DESPESA"
+        onValueChange={(value) => {
+          setPlanningType(value as 'DESPESA' | 'RECEITA')
+          reset()
+          setSelectedTitulo('')
+          setSelectedCategoryId('')
+        }}
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger
+            value="DESPESA"
+            onClick={() => setPlanningType('DESPESA')}
+          >
+            <TrendingDown className="mr-1 h-4 w-4 text-destructive" />
+            Despesa
+          </TabsTrigger>
+          <TabsTrigger
+            value="RECEITA"
+            onClick={() => setPlanningType('RECEITA')}
+          >
+            <TrendingUp className="mr-1 h-4 w-4 text-primary" />
+            Receita
+          </TabsTrigger>
+        </TabsList>
 
-        <form onSubmit={handleSubmit(handlePlanning)}>
-          <CardContent className="space-y-4">
-            <div className="relative">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <FilePen className="h-4 w-4 text-gray-500" />
-              </span>
-              <Input
-                id="titulo"
-                className="pl-10"
-                placeholder="Título"
-                {...register('titulo')}
-                onChange={(e) => setSelectedTitulo(e.target.value)}
-              />
-            </div>
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex items-center text-xl font-semibold">
+              <Presentation className="mr-2 h-5 w-5 text-gray-700" />
+              Planejamento Mensal{' '}
+              {planningType === 'DESPESA' ? (
+                <TrendingDown className="ml-2 h-5 w-5 text-destructive" />
+              ) : (
+                <TrendingUp className="ml-2 h-5 w-5 text-primary" />
+              )}
+            </CardTitle>
+          </CardHeader>
 
-            <div className="relative">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <Banknote className="h-4 w-4 text-gray-500" />
-              </span>
-              <Input
-                id="valorTarget"
-                className="pl-10"
-                placeholder="0,00"
-                {...register('valorTarget', {
-                  required: 'O valor é obrigatório',
-                  validate: (value) =>
-                    parseFloat(value.replace(/\./g, '').replace(',', '.')) >
-                      0 || 'O valor deve ser maior que 0',
-                })}
-                onChange={(e) => {
-                  const maskedValue = insertMaskInMoney(e.target.value)
-                  setValue('valorTarget', maskedValue, {
-                    shouldValidate: true,
-                  })
-                }}
-              />
-            </div>
-
-            <div className="space-y-1">
+          <form onSubmit={handleSubmit(handlePlanning)}>
+            <CardContent className="space-y-4">
               <div className="relative">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <Bookmark className="h-4 w-4 text-gray-500" />
+                  <FilePen className="h-4 w-4 text-gray-500" />
                 </span>
-                <CategorySelector
-                  type={'DESPESA'}
-                  selectedCategory={selectedCategoryId}
-                  onChange={(id) => setSelectedCategoryId(id || '')}
+                <Input
+                  id="titulo"
+                  className="pl-10"
+                  placeholder="Título"
+                  {...register('titulo')}
+                  onChange={(e) => setSelectedTitulo(e.target.value)}
                 />
               </div>
-            </div>
-          </CardContent>
 
-          <CardFooter>
-            <DialogFooter className="w-full">
-              <Button
-                variant="outline"
-                className="w-full"
-                disabled={
-                  isSubmitting ||
-                  !selectedTitulo ||
-                  !selectedCategoryId ||
-                  !watch('valorTarget') ||
-                  parseFloat(
-                    watch('valorTarget').replace(/\./g, '').replace(',', '.'),
-                  ) <= 0
-                }
-              >
-                Adicionar
-              </Button>
-            </DialogFooter>
-          </CardFooter>
-        </form>
-      </Card>
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <Banknote className="h-4 w-4 text-gray-500" />
+                </span>
+                <Input
+                  id="valorTarget"
+                  className="pl-10"
+                  placeholder="0,00"
+                  {...register('valorTarget', {
+                    required: 'O valor é obrigatório',
+                    validate: (value) =>
+                      parseFloat(value.replace(/\./g, '').replace(',', '.')) >
+                        0 || 'O valor deve ser maior que 0',
+                  })}
+                  onChange={(e) => {
+                    const maskedValue = insertMaskInMoney(e.target.value)
+                    setValue('valorTarget', maskedValue, {
+                      shouldValidate: true,
+                    })
+                  }}
+                />
+              </div>
+
+              <div className="space-y-1">
+                <div className="relative">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <Bookmark className="h-4 w-4 text-gray-500" />
+                  </span>
+                  <CategorySelector
+                    type={planningType}
+                    selectedCategory={selectedCategoryId}
+                    onChange={(id) => setSelectedCategoryId(id || '')}
+                  />
+                </div>
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <DialogFooter className="w-full">
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  disabled={
+                    isSubmitting ||
+                    !selectedTitulo ||
+                    !selectedCategoryId ||
+                    !watch('valorTarget') ||
+                    parseFloat(
+                      watch('valorTarget').replace(/\./g, '').replace(',', '.'),
+                    ) <= 0
+                  }
+                >
+                  Adicionar
+                </Button>
+              </DialogFooter>
+            </CardFooter>
+          </form>
+        </Card>
+      </Tabs>
     </DialogContent>
   )
 }
